809e4f1309622eb8a96baccbbe0a7d1ed743aab7
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2022-12-12 17:55:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navigation from './components/Navigation'
+import FixedBackground from './components/FixedBackground'
 
 function App() {
   return (
     <main>
+      <FixedBackground />
       <Navigation />
       <h1>Hello</h1>
     </main>
