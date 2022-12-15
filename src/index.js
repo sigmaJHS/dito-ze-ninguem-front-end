3417fad6ad187c80dd4c20c024d1c181612e34bc
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2022-12-15 17:07:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './style/main.scss';
 
 import App from './App';
 
